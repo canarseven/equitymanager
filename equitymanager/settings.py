ec80762d8dde5734ea9486065d30181ea7bfe222
canarseven <can.arseven@stud.fh-campuswien.ac.at>
2020-08-09 01:57:46 +0200

    Tiny Commit: Added em.arseven.at to allowed hosts

--- equitymanager/settings.py
+++ equitymanager/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["equitymanager.herokuapp.com", "fa.arseven.at", "127.0.0.1"]
+ALLOWED_HOSTS = ["equitymanager.herokuapp.com", "em.arseven.at", "127.0.0.1"]
 
 # Application definition
 
